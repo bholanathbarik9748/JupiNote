c4f2234596a31a17e8e3774ccba34625178c579b
bholanathbarik9748 <bholanathbarik9748@gmail.com>
2023-06-23 00:02:34 +0530

    deploy version v1.0.1

--- src/pages/Intro/index.jsx
+++ src/pages/Intro/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import IntoNavbar from '../../components/into/navbar/navbar';
+import IntoNavbar from '../../components/into/Navbar/Navbar';
 import IntoHero from '../../components/into/Hero/Hero';
 import IntoHero1 from '../../components/into/Hero1/Hero1';
 import IntoTestimonials from '../../components/into/Testimonials/Testimonials';
